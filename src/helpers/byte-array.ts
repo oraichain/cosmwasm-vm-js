f2895c33c1b4ebd93cacc37b76cecf0bc55e69c9
Pham Tu <tubackkhoa@gmail.com>
2023-08-03 21:48:14 +0700

    update helper

--- src/helpers/byte-array.ts
+++ src/helpers/byte-array.ts
@@ -18,18 +18,23 @@ export function compare(bytes1: Uint8Array, bytes2: Uint8Array): number {
 
 export function toNumber(bigEndianByteArray: Uint8Array | number[]) {
   let value = 0;
-  for (let i = 0; i < bigEndianByteArray.length; i++) {
-    value = value * 256 + bigEndianByteArray[i];
+  for (const num of bigEndianByteArray) {
+    value = (value << 8) | num;
   }
   return value;
 }
 
-export function toByteArray(number: number, fixedLength?: number) {
+export function toByteArray(
+  number: number,
+  fixedLength?: number,
+  offset: number = 0
+) {
   // log2(1) == 0, ceil(0) = 0
-  let byteLength = fixedLength ?? (Math.ceil(Math.log2(number) / 8) || 1);
+  const byteLength = fixedLength ?? (Math.ceil(Math.log2(number) / 8) || 1);
   const bytes = new Uint8Array(byteLength);
+  let ind = byteLength - offset;
   while (number > 0) {
-    bytes[--byteLength] = number & 0b11111111;
+    bytes[--ind] = number & 0b11111111;
     number >>= 8;
   }
   return bytes;
