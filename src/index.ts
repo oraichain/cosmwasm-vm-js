e7b4dcc914c0335c94aa90a1407948afca1eaea5
Pham Tu <tubackkhoa@gmail.com>
2023-08-07 18:27:53 +0700

    remove redundant

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
-import { eddsa } from 'elliptic';
-
 export * from './types';
 export * from './memory';
 export * from './backend';
 export * from './instance';
 export * from './environment';
-
-import { VMInstance } from './instance';
-VMInstance.eddsa = new eddsa('ed25519');
