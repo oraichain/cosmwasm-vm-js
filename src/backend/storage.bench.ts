3702cb61a875c16ed4715527ed12aa8e625aff66
Pham Tu <tubackkhoa@gmail.com>
2023-08-09 10:43:33 +0700

    update benchmark

--- src/backend/storage.bench.ts
+++ src/backend/storage.bench.ts
@@ -1,39 +1,43 @@
 // @ts-nocheck
 
-import { fromBase64, toAscii } from '@cosmjs/encoding';
+import { toAscii } from '@cosmjs/encoding';
 import { toByteArray, toNumber } from '../helpers/byte-array';
 import { BasicKVIterStorage, SortedKVIterStorage, Order } from './storage';
 
 let store = new BasicKVIterStorage();
 let fastStore = new SortedKVIterStorage();
-for (let i = 0; i < 10000; ++i)
+console.time('BasicKVIterStorage Insert');
+for (let i = 0; i < 1000000; ++i)
   store.set(toByteArray(i, 4), toAscii(i.toString()));
-for (let i = 0; i < 10000; ++i)
+console.timeEnd('BasicKVIterStorage Insert');
+console.time('SortedKVIterStorage Insert');
+for (let i = 0; i < 1000000; ++i)
   fastStore.set(toByteArray(i, 4), toAscii(i.toString()));
+console.timeEnd('SortedKVIterStorage Insert');
 
-let start = toByteArray(100, 4);
-let stop = toByteArray(110, 4);
+let start = toByteArray(500000, 4);
+let stop = toByteArray(500010, 4);
 
-console.time('BasicKVIterStorage');
-for (let i = 0; i < 100; ++i)
-  store.all(store.scan(start, stop, Order.Descending));
-console.timeLog(
-  'BasicKVIterStorage',
+console.time('BasicKVIterStorage Scan');
+store.all(store.scan(start, stop, Order.Ascending));
+console.timeEnd('BasicKVIterStorage Scan');
+
+console.log(
   store
-    .all(store.scan(start, stop, Order.Descending))
+    .all(store.scan(start, stop, Order.Ascending))
     .map((record) => [
       toNumber(record.key),
       Buffer.from(record.value).toString(),
     ])
 );
 
-console.time('SortedKVIterStorage');
-for (let i = 0; i < 100; ++i)
-  fastStore.all(fastStore.scan(start, stop, Order.Descending));
-console.timeLog(
-  'SortedKVIterStorage',
+console.time('SortedKVIterStorage Scan');
+fastStore.all(fastStore.scan(start, stop, Order.Ascending));
+console.timeEnd('SortedKVIterStorage Scan');
+
+console.log(
   fastStore
-    .all(fastStore.scan(start, stop, Order.Descending))
+    .all(fastStore.scan(start, stop, Order.Ascending))
     .map((record) => [
       toNumber(record.key),
       Buffer.from(record.value).toString(),
